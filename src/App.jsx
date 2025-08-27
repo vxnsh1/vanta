13c783f24680b1ebacf0d5b9f139fef1b9796a01
Vansh Chouhan <dragonnbutded@gmail.com>
2025-08-27 23:43:54 +0530

    Added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home"
+import Agence from "./pages/Agence"
+import Projects from "./pages/Projects"
 
 const App = () => {
   return (
-    <div className=''>
-      
+    <div className='text-white'>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/agence' element={<Agence/>} />
+        <Route path='/project' element={<Projects/>} />
+      </Routes>
     </div>
   )
 }
